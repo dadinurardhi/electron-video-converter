fa36f0e68191f059b5e29d50370541d8fd302583
Ardhi <dadinurardhi@gmail.com>
2022-06-07 17:26:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
+
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+      backgroundThrottling: false,
+    },
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on('video:added', (event, videos) => {
+  const promises = _.map(videos, (video) => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (error, metadata) => {
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
+      });
+    });
+  });
+
+  Promise.all(promises).then((results) => {
+    mainWindow.webContents.send('metadata:complete', results);
+  });
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+  const video = videos[0];
+  // const outputDirectory = video.path.split(video.name)[0];
+  console.log(video);
+  // ffmpeg(video.path).output();
+});
